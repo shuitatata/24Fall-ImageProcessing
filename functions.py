080f991d0d46a653bfae34f3f2bb2be318f23be0
shuitata <2462703357@qq.com>
2024-10-20 15:23:15 +0800

    Init repo

---
+++ functions.py
@@ -0,0 +1,38 @@
+#-*-coding:GBK -*-
+import numpy as np
+import gradio as gr
+
+def function_hw1(input_image):
+    if input_image is None:
+        raise gr.Error('输入错误：在处理之前请先输入一张图像', duration=5)
+    output_image = input_image
+    # 请补充作业1的图像处理代码
+    return output_image
+
+def function_hw2(input_image):
+    if input_image is None:
+        raise gr.Error('输入错误：在处理之前请先输入一张图像', duration=5)    
+    output_image = input_image
+    # 请补充作业2的图像处理代码
+    return output_image
+
+def function_hw3(input_image):
+    if input_image is None:
+        raise gr.Error('输入错误：在处理之前请先输入一张图像', duration=5)   
+    output_image = input_image
+    # 请补充作业3的图像处理代码
+    return output_image
+
+def function_hw4(input_image):
+    if input_image is None:
+        raise gr.Error('输入错误：在处理之前请先输入一张图像', duration=5)
+    output_image = input_image
+    # 请补充作业4的图像处理代码
+    return output_image
+
+def function_hw5(input_image):
+    if input_image is None:
+        raise gr.Error('输入错误：在处理之前请先输入一张图像', duration=5)
+    output_image = input_image
+    # 请补充作业5的图像处理代码
+    return output_image
